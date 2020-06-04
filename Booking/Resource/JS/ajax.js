0fcee913e94bc4a84b63e44bbb5ea6a948b15437
VAC <37587399+vacnex@users.noreply.github.com>
2020-06-04 20:59:44 +0700

    Add div loader when room is loading

--- Booking/Resource/JS/ajax.js
+++ Booking/Resource/JS/ajax.js
@@ -1,7 +1,23 @@
 $(document).ready(function () {
+    $('.loading').hide();
     $('.btnstandar').click(function (e) { 
-        $.getJSON("https://localhost:44373/api/room",
-            function (data) {
+        // $.getJSON("https://localhost:44373/api/room",
+        //     function (data) {
+        //     var _room = data;
+        //     console.log(_room)
+        //     _room.forEach(function (arritem) {
+        //         var idRoom = arritem.idRoom
+        //         var rStatus = arritem.rStatus
+        //         if(idRoom.startsWith("L")==false && idRoom.startsWith("V")==false && rStatus.startsWith("T")==true)
+        //         {
+        //             $('.posroom').append('<div class="col mb-4"><div class="card"> <div class="text-center"><img src="Resource/image/bed.svg" style="width: 100px;height: 100px";" class="card-img-top" alt="..."></div> <div class="card-body"> <h5 class="card-title">Mã phòng: '+arritem.idRoom+'</h5> <h5 class="card-title">Loại: '+arritem.rType+'</h5> <h5 class="card-title">Giá: '+arritem.rPrice+' VND</h5> </div> </div></div>');
+        //         }
+        //     });
+        // });
+
+        $('.loading').show();
+        $.getJSON("https://localhost:44373/api/room")
+        .done(function (data) { 
             var _room = data;
             console.log(_room)
             _room.forEach(function (arritem) {
@@ -12,7 +28,16 @@ $(document).ready(function () {
                     $('.posroom').append('<div class="col mb-4"><div class="card"> <div class="text-center"><img src="Resource/image/bed.svg" style="width: 100px;height: 100px";" class="card-img-top" alt="..."></div> <div class="card-body"> <h5 class="card-title">Mã phòng: '+arritem.idRoom+'</h5> <h5 class="card-title">Loại: '+arritem.rType+'</h5> <h5 class="card-title">Giá: '+arritem.rPrice+' VND</h5> </div> </div></div>');
                 }
             });
-        });
+        })
+        .always(function () { $(".loading").hide(); });
+        
+
+
+
+
+
+
+
     });
     /* #region  Frontend */
     $('.card').hover(function () {
@@ -26,7 +51,7 @@ $(document).ready(function () {
             marginBottom :"0%",
         },150);
     });
-    
+
     $('#startday').datepicker({
         uiLibrary: 'bootstrap4'
     });
