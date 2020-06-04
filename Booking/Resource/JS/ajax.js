3f894088cd5e991f43cff60bbc0a97cf3b01a0f8
VAC <37587399+vacnex@users.noreply.github.com>
2020-06-04 20:10:13 +0700

    Booking UI

--- Booking/Resource/JS/ajax.js
+++ Booking/Resource/JS/ajax.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+/* #region  Frontend */
     $('.card').hover(function () {
             $(this).animate({
                 marginTop : "-=1%",
@@ -12,4 +13,21 @@ $(document).ready(function () {
             },150);
         }
     );
+    $('#startday').datepicker({
+        uiLibrary: 'bootstrap4'
+    });
+    $('#endday').datepicker({
+        uiLibrary: 'bootstrap4'
+    });
+/* #endregion */
+
+    $('.btnstandar').click(function (e) { 
+        $.getJSON("https://localhost:44373/api/room",
+            function (data) {
+                var _room = data;
+                console.log(_room)
+            }
+        );
+        
+    });
 });
