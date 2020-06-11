0e1f6e067a12c1471be745b4b4fba35d6199ebf0
pinskynet <33683878+pinskynet@users.noreply.github.com>
2020-06-11 12:10:17 +0700

    First Upload

---
+++ QuanLyThuePhong/QuanLyThuePhong/DangNhap/Frm_DangNhap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThuePhong.DangNhap
+{
+    public partial class Frm_DangNhap : Form
+    {
+        public Frm_DangNhap()
+        {
+            InitializeComponent();
+        }
+        TaiKhoanFs _taiKhoanFs;
+        DTO_TaiKhoan _dTO_TaiKhoan;
+        private void Frm_DangNhap_Load(object sender, EventArgs e)
+        {
+            _taiKhoanFs = new TaiKhoanFs();
+            _dTO_TaiKhoan = new DTO_TaiKhoan();
+        }
+        private void btn_DangNhap_Click(object sender, EventArgs e)
+        {
+            _dTO_TaiKhoan = _taiKhoanFs.GetTaiKhoanTheoTenDangNhap(txt_TenDangNhap.Text);
+
+            if (txt_TenDangNhap.Text == _dTO_TaiKhoan.Username && txt_MatKhau.Text == _dTO_TaiKhoan.Pass)
+            {
+                this.Close();
+                MessageBox.Show("Đăng nhập thành công!!!");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại!!!");
+            }
+        }
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }        
+    }
+}
